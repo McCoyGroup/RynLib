b0670b5f308cf716658d3609112512ea614e7c7e
b3m2a1 <b3m2a1@gmail.com>
2020-04-08 23:59:27 -0700

    I'm so duuuumb ;________;

--- setup/build_singularity.sh
+++ setup/build_singularity.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+
+cur=$PWD
 cd $(dirname $0)
 
 singularity build --fakeroot --docker-login entos SingularityEntos.def
 singularity build --fakeroot rynlib Singularity.def
 
-mv rynlib ../../rynlib
+mv rynlib cur/rynlib
+cd $cur
+./rynlib config reload_dumpi
