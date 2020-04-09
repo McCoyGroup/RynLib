579efe9859a37af4e8f1e3563464401a4af5b241
b3m2a1 <b3m2a1@gmail.com>
2020-04-09 01:17:05 -0700

    I think this error handler can't be set?

--- Dumpi/src/Dumpi.cpp
+++ Dumpi/src/Dumpi.cpp
@@ -9,11 +9,11 @@ void _mpiInit(int* world_size, int* world_rank) {
     if (!did_i_do_good_pops){
 //        int error;
 //        error = MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
-        printf("this is before MPI_Init\n");
+//        printf("this is before MPI_Init\n");
 //        MPI_Errhandler_set(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
 //        MPI_ERRORS_ARE_FATAL = MPI_ERRORS_RETURN; // eesh this is dangerous, but I just want to see if it works...
         err = MPI_Init(NULL, NULL);
-        printf("...okay I guess MPI initialized\n");
+//        printf("...okay I guess MPI initialized\n");
 //        error = MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_ARE_FATAL);
     };
     if (err == MPI_SUCCESS) {
