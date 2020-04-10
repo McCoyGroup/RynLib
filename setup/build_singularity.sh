1e1976fd7eb397e959caaac9d70f3bfc48a8ca4f
b3m2a1 <b3m2a1@gmail.com>
2020-04-09 20:55:15 -0700

    ugh

--- setup/build_singularity.sh
+++ setup/build_singularity.sh
@@ -12,8 +12,8 @@ cd $cur
 mpi_lib="$1"
 if [[ "$mpi_lib" == "" ]]; then
   module load gcc_8.2.1-ompi_3.1.4
-  unset CC
-  unset CXX
+  CC=/config/libs/mpi/bin/mpicc
+  CC=/config/libs/mpi/bin/mpic++
   mpi_lib="/sw/openmpi/3.1.4-gcc-8.2.1/"
 fi
 rynlib="singularity run --bind .:/config,$mpi_lib:/config/libs/mpi rynlib"
