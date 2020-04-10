232aae23e99b15c8d80b6173d161a4c6d0821a8e
b3m2a1 <b3m2a1@gmail.com>
2020-04-09 19:31:10 -0700

    New MPI model

--- setup/build_singularity.sh
+++ setup/build_singularity.sh
@@ -8,4 +8,10 @@ singularity build --fakeroot rynlib Singularity.def
 
 mv rynlib $cur/rynlib
 cd $cur
-./rynlib config reload_dumpi
+# this is Hyak specific, for now...
+mpi_lib="$1"
+if [[ "$mpi_lib" == "" ]]; then
+  mpi_lib="/sw/openmpi/3.1.4-gcc-8.2.1/"
+fi
+rynlib="singularity run --bind $mpi_lib:libs/mpi rynlib"
+$rynlib config reload_dumpi
