bc07115e2a1c4302963cbc3e2b1db4fafc20eac2
b3m2a1 <b3m2a1@gmail.com>
2020-04-21 12:49:57 -0700

    sad

--- setup/build_singularity.sh
+++ setup/build_singularity.sh
@@ -3,7 +3,8 @@
 cur=$PWD
 cd $(dirname $0)
 
-singularity build --fakeroot --docker-login entos SingularityEntos.def
+#singularity build --fakeroot --docker-login entos SingularityEntos.def
+singularity build --fakeroot core SingularityCore.def
 singularity build --fakeroot rynlib Singularity.def
 
 cp rynlib $cur/rynlib
